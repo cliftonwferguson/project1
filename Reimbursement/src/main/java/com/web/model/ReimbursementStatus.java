c0c5e0b96f03b23f18d1d6146eb59dffe0fb1c84
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-17 19:06:01 -0500

    reimstatus gen

--- Reimbursement/src/main/java/com/web/model/ReimbursementStatus.java
+++ Reimbursement/src/main/java/com/web/model/ReimbursementStatus.java
@@ -2,4 +2,41 @@ package com.web.model;
 
 public class ReimbursementStatus {
 
+	/*
+	 * create table project1.ers_reimbursement_status (
+    reimb_status_id serial primary key,
+    reimb_status text)
+	 */
+	
+	private int statusid;
+	private String status;
+	
+	public int getStatusid() {
+		return statusid;
+	}
+	public void setStatusid(int statusid) {
+		this.statusid = statusid;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "ReimbursementStatus [statusid=" + statusid + ", status=" + status + "]";
+	}
+	public ReimbursementStatus() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public ReimbursementStatus(int statusid, String status) {
+		super();
+		this.statusid = statusid;
+		this.status = status;
+	}
+	
+	
+	
 }
