c2386756e3c58095b5629651f076a9bfb43f546a
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-20 23:19:07 -0500

    need to adjust models

--- project1Script-9.sql
+++ project1Script-9.sql
@@ -4,6 +4,18 @@ GRANT ALL ON TABLE project1.ers_reimbursement_type TO jdbc_worker;
 GRANT ALL ON TABLE project1.ers_user_roles TO jdbc_worker;
 GRANT ALL ON TABLE project1.ers_users TO jdbc_worker;
 
+select a.ers_users_id, a.ers_username, a.ers_password, a.user_first_name, a.user_last_name, a.user_email, b.user_role 
+from ers_users a join ers_user_roles b on a.user_role_id = b.ers_user_role_id;
+
+drop view reimbursement_view;
+
+create view reimbursement_view as select a.reimb_id, a.reimb_author, a.reimb_amount, 
+    b.reimb_status, a.reimb_submitted, c.reimb_type 
+    from ers_reimbursement a join ers_reimbursement_status b on a.reimb_id  =  b.reimb_status_id
+   join ers_reimbursement_type c on a.reimb_id = c.reimb_type_id;
+  
+  select * from reimbursement_view;
+
 create table project1.ers_users
 
 drop table project1.ers_reimbursement;
@@ -25,6 +37,9 @@ CREATE TABLE project1.ers_reimbursement (
 	reimb_status_id integer references public.ers_reimbursement_status(reimb_status_id),
 	reimb_type_id integer references public.ers_reimbursement_type(reimb_type_id)
 );
+
+   insert into project1.ers_reimbursement (reimb_amount, reimb_description, reimb_receipt, reimb_author, reimb_resolver, reimb_status_id, reimb_type_id) values
+     (100, 'travel', ('/home/xyz')::bytea, 2, 1, 1, 3);
 
 select * from project1.ers_reimbursement_status;
 
@@ -51,7 +66,9 @@ drop table project1.ers_user_roles;
 
 insert into project1.ers_user_roles(user_role) values ('Employee'), ('Admin'), ('Supervisor'); 
 
-DELETE FROM project1.ers_user_roles WHERE user_role='employee';
+
+
+select * from user
 
 CREATE TABLE project1.ers_user_roles (
 	ers_user_role_id serial primary key,
@@ -73,4 +90,8 @@ create table project1.ers_users (
   unique(user_email),
   user_role_id integer references public.ers_user_roles(ers_user_role_id)  
  )
+ 
+ insert into project1.ers_users (ers_users_id, ers_username, ers_password, user_first_name, user_last_name, user_email, user_role_id) values
+     (1, 'adam.adams', 'qwe', 'adam', 'adam', 'adam.adams@email.com', 2),
+     (2, 'John.smith', 'asd', 'john', 'smith', 'john.smith@email.com', 1);
 
