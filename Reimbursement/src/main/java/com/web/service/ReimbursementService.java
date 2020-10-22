75ef64a45b82546cf0e4b92cf5ef261eeeebe8bf
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-21 22:11:32 -0500

    building the reim service

--- Reimbursement/src/main/java/com/web/service/ReimbursementService.java
+++ Reimbursement/src/main/java/com/web/service/ReimbursementService.java
@@ -1,6 +1,11 @@
 package com.web.service;
 
+import com.web.model.Reimbursement;
+import com.web.repo.DaoContract;
+
 public class ReimbursementService {
    
+	private DaoContract<Reimbursement, Integer>
+	
 	
 }
