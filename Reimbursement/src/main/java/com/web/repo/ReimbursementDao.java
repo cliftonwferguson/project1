59c5324164cb35ca3b6ab6470b942df2b3496fea
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-21 19:36:01 -0500

    stubbed out reimbdao

--- Reimbursement/src/main/java/com/web/repo/ReimbursementDao.java
+++ Reimbursement/src/main/java/com/web/repo/ReimbursementDao.java
@@ -1,5 +1,39 @@
 package com.web.repo;
 
-public class ReimbursementDao {
+import java.util.List;
+
+import com.web.model.Reimbursement;
+
+public class ReimbursementDao implements DaoContract<Reimbursement, Integer>{
+
+	@Override
+	public List<Reimbursement> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reimbursement findById(Integer i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Reimbursement t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int create(Reimbursement t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Reimbursement i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
