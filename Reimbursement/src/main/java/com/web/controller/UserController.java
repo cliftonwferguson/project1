55b4c7852f493e57fa71448aa748dba96fa1aba9
cliftonwferguson <Clifton.ferguson7@gmail.com>
2020-10-24 12:21:55 -0500

    adjusted landing user controler for fbn

--- Reimbursement/src/main/java/com/web/controller/UserController.java
+++ Reimbursement/src/main/java/com/web/controller/UserController.java
@@ -1,5 +1,35 @@
 package com.web.controller;
 
-public class UserController {
+import java.util.Optional;
 
-}
+import javax.servlet.http.HttpServletRequest;
+
+import com.web.model.User;
+import com.web.service.UserService;
+
+public class UserController {
+	
+	private UserService us;
+	
+	public UserController(UserService us) {
+		super();
+		this.us = us;
+	}
+	
+	public UserController() {
+		this(new UserService());
+	}
+	
+	public String login(HttpServletRequest req) {
+		String userName = req.getParameter("name");
+		User u = us.findByName(userName);
+		if(Optional.of(u).isPresent()) {
+			return "html/user.html";
+		} else {
+			return "html/landing.html";
+		}
+			
+		}
+	}
+
+
